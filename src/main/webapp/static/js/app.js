b1f0119854467396e4ec9ef03af58b996983e9d2
Eqan Butt <eqan.asif@gmail.com>
2017-08-07 16:03:14 -0400

    Adding a basic loading ui and set initial loadign to New York

--- src/main/webapp/static/js/app.js
+++ src/main/webapp/static/js/app.js
@@ -2,11 +2,15 @@
 
 (function() {
 
-	const baseUri = window.location.pathname
+	const BASE_URI = window.location.pathname
 
-	const CITIES_SEARCH_URI = baseUri + "api/cities";
-	const RAIN_PREDICTION_URI = baseUri + "api/predict";
-	
+	const CITIES_SEARCH_URI = BASE_URI + "api/cities";
+	const RAIN_PREDICTION_URI = BASE_URI + "api/predict";
+	const DEFAULT_INPUT_VALUE = "New York, New York";
+	const DEFAULT_TYPEAHEAD_OBJECT = {
+		canonicalName : DEFAULT_INPUT_VALUE
+	};
+
 	$(".typeahead").typeahead({
 		hint : true,
 		highlight : true,
@@ -14,9 +18,9 @@
 
 	}, {
 		source : function(query, syncResults, asyncResults) {
-			if(query.trim().length === 0) {
+			if (query.trim().length === 0) {
 				return;
-			} 
+			}
 			$.get(CITIES_SEARCH_URI, {
 				query : query
 			}, asyncResults);
@@ -28,43 +32,44 @@
 		templates : {
 			notFound : "<div>No results found</div>"
 		}
-	});
+	}).typeahead("val", DEFAULT_INPUT_VALUE);
 
 	$(".typeahead").bind("typeahead:select", function(ev, suggestion) {
+
 		getRainPrediction(suggestion.canonicalName).then(showHourlyData);
-	});
+	}).trigger("typeahead:select", DEFAULT_TYPEAHEAD_OBJECT);
+
 	$(".typeahead").bind("typeahead:autocompleted", function(ev, suggestion) {
 		getRainPrediction(suggestion.canonicalName).then(showHourlyData);
 		$(this).typeahead('close');
 	});
-	
+
 	function showHourlyData(data) {
-		
+
 		const hourlyData = data.hourly.data.slice(1);
 		hourlyData.unshift(data.currently);
-		
+
 		const table = $("#probTable")
-		
+
 		hourlyData.map(function(current, index) {
-			
+
 			const row = $("<tr></tr>");
 			row.appendTo(table);
-			
+
 			const TD_HTML = "<td></td>";
-			
+
 			const timeCell = $(TD_HTML);
 			if (index === 0) {
 				timeCell.text("Now");
 			} else {
-				timeCell.text(new Date(current.time*1000).toLocaleString());
+				timeCell.text(new Date(current.time * 1000).toLocaleString());
 			}
 			timeCell.appendTo(row);
-			
-			
+
 			const summaryCell = $(TD_HTML);
 			summaryCell.text(current.summary);
 			summaryCell.appendTo(row);
-			
+
 			const probCell = $(TD_HTML);
 			probCell.text(current.precipProbability);
 			probCell.appendTo(row);
@@ -72,17 +77,23 @@
 		});
 
 	}
-	
+
 	function getRainPrediction(location) {
-		
-		return $.get(RAIN_PREDICTION_URI, { city: location}, function(data) {
-		
-		const summaryCells = $("#summaryTable tbody tr:first td");
-		summaryCells.eq(0).text(data.hourly.summary);
-		summaryCells.eq(1).text(data.currently.summary);
-		
+
+		const loadingDiv = $(".loading");
+		loadingDiv.css("display", "");
+
+		return $.get(RAIN_PREDICTION_URI, {
+			city : location
+		}, function(data) {
+			loadingDiv.css("display", "none");
+			
+			const summaryCells = $("#summaryTable tbody tr:first td");
+			summaryCells.eq(0).text(data.hourly.summary);
+			summaryCells.eq(1).text(data.currently.summary);
+
 		});
-		
+
 	}
-	
+
 })();

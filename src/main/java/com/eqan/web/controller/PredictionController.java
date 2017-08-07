1f9e6a34bc06dceb0d495bf6fc27970a1ba35fb1
Eqan Butt <eqan.asif@gmail.com>
2017-08-07 16:54:43 -0400

    Add logging for prediction requests

--- src/main/java/com/eqan/web/controller/PredictionController.java
+++ src/main/java/com/eqan/web/controller/PredictionController.java
@@ -1,5 +1,7 @@
 package com.eqan.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +14,7 @@ import com.eqan.web.service.Predictor;
 @RestController
 @RequestMapping("/api")
 public class PredictionController {
-	
+	private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
 	@Autowired
 	Predictor predictor;
 	
@@ -20,6 +22,9 @@ public class PredictionController {
 	public PredictionResult getPrediction(@RequestParam String city) {
 		if (city.trim().isEmpty()) {
 			throw new IllegalArgumentException("The city parameter must not be null or empty");
+		}
+		if(LOG.isDebugEnabled()) {
+			LOG.debug("Received request for /predict for city " + city);
 		}
 		return predictor.predict(city);
 	}

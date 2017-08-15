15a7ac7eb058ed07e587adb11886a62a6b06d3aa
Eqan Butt <eqan.asif@gmail.com>
2017-08-15 11:39:07 -0400

    Refactored darknet predictor to read api key from properties file

--- src/main/java/com/eqan/web/service/SimplePredictor.java
+++ src/main/java/com/eqan/web/service/SimplePredictor.java
@@ -8,10 +8,10 @@ import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.eqan.web.model.PredictionResult;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -19,13 +19,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service("simplePredictor")
 public class SimplePredictor implements Predictor {
-	private static String BASE_URL = "https://api.darksky.net/forecast/b7a7e9efac3718aace6b59265fb8d0b9/{lat},{lng}";
-	private static String QUERY_PARAM = "exclude";
-	private static String PARAM_VALUE = "minutely,daily,alerts,flags";
+	private static String BASE_URL = "https://api.darksky.net/forecast/{API_KEY}/{lat},{lng}?exclude={EXCLUDE_DATAPOINTS}";
+	private static String EXCLUDE_DATAPOINTS = "minutely,daily,alerts,flags";
 	private static final Logger LOG = LoggerFactory.getLogger(SimplePredictor.class);
 
 	@Autowired
 	private CoordinateRetrieval coordinateService;
+	
+	@Value("${DARKSKY_API_KEY}")
+	private String API_KEY;
 
 	@Override
 	public PredictionResult predict(String city) {
@@ -34,10 +36,9 @@ public class SimplePredictor implements Predictor {
 
 			Map<String, Double> coordinates = coordinateService.getCoordinatesByCity(city);
 
-			String requestURL = UriComponentsBuilder.fromUriString(BASE_URL).queryParam(QUERY_PARAM, PARAM_VALUE)
-					.buildAndExpand(coordinates).toUriString();
 			RestTemplate restTemplate = new RestTemplate();
-			ResponseEntity<String> response = restTemplate.getForEntity(requestURL, String.class);
+            ResponseEntity<String> response = restTemplate.getForEntity(BASE_URL, String.class, API_KEY,
+                    coordinates.get("lat"), coordinates.get("lng"), EXCLUDE_DATAPOINTS);
 			ObjectMapper mapper = new ObjectMapper();
 			JsonNode jsonNode = mapper.readTree(response.getBody());
 			result = mapper.treeToValue(jsonNode, PredictionResult.class);

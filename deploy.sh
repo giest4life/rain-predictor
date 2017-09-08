b87553501de2585b9163075513b11b9814252dbf
Eqan Butt <eqan.asif@gmail.com>
2017-09-08 08:31:57 -0400

    Enhanced deployment script to echo and remove old war file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+WEBAPP_FOLDER=/opt/tomcat/webapps
+APP_NAME=rain-predictor
+APP_PATH=${WEBAPP_FOLDER}/${APP_NAME}
+
+echo "Deleting $APP_PATH*"
+rm -rf ${APP_PATH}*
+
+echo "Copying war file into $WEBAPP_FOLDER"
 for f in target/*.war
 do
-  cp -v "$f" /opt/tomcat/webapps/rain-predictor.war
+  cp -v "$f" ${APP_PATH}.war 
 done
